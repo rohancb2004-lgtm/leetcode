e79b1a0ba0a2f6f480ed9f154538a180cf0ecfa8
Rohan C Biju <rohancbiju@Rohans-MacBook-Air.local>
2026-07-14 00:40:51 +0530

    commit

---
+++ python/0001_Two_Sum/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if nums[i] in d:
+                return[d[nums[i]],i]
+            d[diff]=i
+
+
+    
+
+
+        
